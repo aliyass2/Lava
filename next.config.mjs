736f8aadc0185efeb3a5e7ec1bf0cb28828fe2d9
Aliyass21 <ali.yaas@scopesky.iq>
2025-04-24 10:18:54 +0300

    filesize

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      // simple allow‑list
-      domains: ['res.cloudinary.com'],
+  reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '20mb',   // ← bump this to whatever you need
     },
-  }
-  
-  export default nextConfig
+  },
+}
+
+export default nextConfig
